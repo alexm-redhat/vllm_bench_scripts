706ebba0a76fb9515202a69559754c82af9ca14e
Alexander Matveev <amatveev@redhat.com>
2026-01-25 15:59:58 -0500

    fixes

--- trt/trt_config.sh
+++ trt/trt_config.sh
@@ -1,3 +1,6 @@
+# TRT
+export TRT_IMAGE=nvcr.io/nvidia/tensorrt-llm/release:1.2.0rc8
+
 # Model
 export MODEL=nvidia/DeepSeek-R1-NVFP4
 
@@ -11,9 +14,15 @@ export NUM_SAMPLES=1000
 export CONCURRENCY_LIST="1 16"
 
 # Dirs
+export HOST_TRT_DIR="/home/alexm-redhat/code/vllm_bench_scripts/trt"
+export CONTAINER_TRT_DIR="/root/trt"
+
+export PROFILES_DIR="$CONTAINER_TRT_DIR/profiles"
+
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 CUR_DIR="run_$TIMESTAMP"
-export WORK_DIR=/root/profiles/${CUR_DIR}
+
+export WORK_DIR=$PROFILES_DIR/$CUR_DIR
 
 export DATASET_DIR="dataset"
 export MODEL_DIR=$(echo "$MODEL" | sed 's/\//__/g')
--- trt/trt_prepare.sh
+++ trt/trt_prepare.sh
@@ -2,16 +2,30 @@
 
 source config.sh
 
-echo "Create directories in: $WORK_DIR"
+DIR="/path/to/your/directory"
+
+# Sanity check
+if [ -d "$PROFILES_DIR" ]; then
+    echo "Directory found: $PROFILES_DIR"
+else
+    echo "Error: Directory not found - $PROFILES_DIR"
+    echo "Exiting script."
+    exit 1
+fi
+
+# Create dirs
+echo "Create directory: $WORK_DIR"
 mkdir ${WORK_DIR}
+echo "Create directory: ${WORK_DIR}/${DATASET_DIR}"
 mkdir ${WORK_DIR}/${DATASET_DIR}
+echo "Create directory: ${WORK_DIR}/${MODEL_DIR}"
 mkdir ${WORK_DIR}/${MODEL_DIR}
 
+# Run prepare dataset
 echo "Prepare TRT dataset for:"
 echo "  MODEL      = ${MODEL}"
 echo "  INPUT_LEN  = ${INPUT_LEN}"
 echo "  OUTPUT_LEN = ${OUTPUT_LEN}"
-
 python3 benchmarks/cpp/prepare_dataset.py \
 	--tokenizer=${MODEL} \
 	--stdout token-norm-dist \
