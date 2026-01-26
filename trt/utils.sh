a4c33e65c8649d28d5e114c311056e5af9d161d5
Alexander Matveev <amatveev@redhat.com>
2026-01-26 20:11:32 +0000

    add file

---
+++ trt/utils.sh
@@ -0,0 +1,49 @@
+
+_log() {
+  local level="$1"
+  shift
+  local message="$*"
+  local timestamp
+
+  timestamp="$(date '+%Y-%m-%d %H:%M:%S')"
+  echo "$timestamp [$level] $message"
+}
+
+log_info() {
+  _log INFO "$@"
+}
+
+log_warn() {
+  _log WARN "$@"
+}
+
+log_error() {
+  _log ERROR "$@" >&2
+}
+
+log_debug() {
+  _log DEBUG "$@"
+}
+
+create_dir_if_missing() {
+  local dir="$1"
+
+  if [[ -z "$dir" ]]; then
+    log_error "No directory path provided"
+    return 1
+  fi
+
+  log_info "Checking directory: $dir"
+
+  if [[ -d "$dir" ]]; then
+    log_info "  -- Directory already exists: $dir"
+    return 0
+  fi
+
+  log_info "  -- Directory does not exist, attempting to create: $dir"
+
+  mkdir -p "$dir" || {
+    log_error "  -- Failed to create directory: $dir"
+    return 1
+  }
+}
