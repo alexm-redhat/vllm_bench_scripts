72d34f8fb66ab45a5270cef28b0c7660709d4c54
Alexander Matveev <amatveev@redhat.com>
2026-01-26 11:06:51 -0500

    add file

---
+++ trt/trt_run_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source trt_config.sh
+
+CONTAINER_NAME=trt_alex
+
+if docker container inspect $CONTAINER_NAME >/dev/null 2>&1; then
+	docker container stop $CONTAINER_NAME
+	docker container rm $CONTAINER_NAME
+fi
+
+docker run \
+	-it \
+	--ipc=host \
+	--ulimit memlock=-1 \
+	--ulimit stack=67108864 \
+	--gpus=all \
+	-v $HOST_TRT_DIR:$CONTAINER_TRT_DIR \
+	-v $HF_HUB_CACHE:/root/hf_hub_cache \
+	--env "HF_HUB_CACHE=/root/hf_hub_cache" \
+	-p 30000:30000 \
+	--name $CONTAINER_NAME \
+	--rm \
+	$TRT_IMAGE \
+	bash -c "cd $CONTAINER_TRT_DIR; time ./trt_run.sh"
